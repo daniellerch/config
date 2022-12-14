dc47d8b5fb62f1e35551930cc35baba6553aac48
Daniel Lerch <dlerch@gmail.com>
2022-12-14 12:27:30 +0100

    readling

--- create_links.sh
+++ create_links.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p ~/.vim/plugin
-ln -s `pwd`/vim/config.vim ~/.vim/plugin/config.vim
-ln -s `pwd`/tmux/tmux.conf ~/.tmux.conf
+ln -sf `pwd`/vim/config.vim ~/.vim/plugin/config.vim
+ln -sf `pwd`/tmux/tmux.conf ~/.tmux.conf
+ln -sf `pwd`/readline/inputrc ~/.inputrc
+bash
 
