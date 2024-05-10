0b983161e602703f8499655d553e021c606a9e42
Daniel Lerch Hostalot <dlerch@gmail.com>
2024-05-10 09:03:29 +0200

    usual problems

--- create_links.sh
+++ create_links.sh
@@ -2,6 +2,6 @@
 mkdir -p ~/.vim/plugin
 ln -sf `pwd`/vim/config.vim ~/.vim/plugin/config.vim
 ln -sf `pwd`/tmux/tmux.conf ~/.tmux.conf
-ln -sf `pwd`/readline/inputrc ~/.inputrc
+#ln -sf `pwd`/readline/inputrc ~/.inputrc
 bash
 
